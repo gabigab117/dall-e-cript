44cf1475c27e2a7a1e7089d0e10510ccd30c9abd
gabigab117 <gabrieltrouve5@gmail.com>
2024-02-15 18:57:06 +0100

    add docstrings

--- img_generator.py
+++ img_generator.py
@@ -6,12 +6,26 @@ load_dotenv()
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
 
 class ImageGenerator:
+    """
+    A class for generating images using OpenAI's API based on given prompts and model.
+    """
     def __init__(self, model, prompt):
+        """
+        Initializes the image generator with a specific model and text prompt.
+
+        :param model: The name of the model to use for generating images.
+        :param prompt: The text prompt based on which the image will be generated.
+        """
         self.client = OpenAI()
         self.model = model
         self.prompt = prompt
 
     def generate(self):
+        """
+        Generates an image based on the initialized model and prompt.
+
+        :return: The API response containing the generated image.
+        """
         response = self.client.images.generate(
         model=self.model,
         prompt=self.prompt,
@@ -22,4 +36,10 @@ class ImageGenerator:
         return response
     
     def display_url(self, response):
+        """
+        Extracts and returns the URL of the generated image from the API response.
+
+        :param response: The API response from which the image URL is to be extracted.
+        :return: The URL of the generated image.
+        """
         return response.data[0].url
